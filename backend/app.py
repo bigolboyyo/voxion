011bd9b72a9ed4044749751313bfa4100af60af1
bigolboyyo <wesley.grant94@gmail.com>
2023-02-21 16:25:46 -0500

    env/api setup

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/completions', methods=['POST'])
+def generate_completion():
+    # Implement OpenAI completions logic here
+    return jsonify({"result": "TODO"})
+
+@app.route('/models', methods=['GET'])
+def list_models():
+    # Implement OpenAI models listing logic here
+    return jsonify({"result": "TODO"})
+
+@app.route('/parameters', methods=['GET'])
+def list_parameters():
+    # Implement OpenAI parameters listing logic here
+    return jsonify({"result": "TODO"})
+
+@app.route('/status', methods=['GET'])
+def get_status():
+    # Implement OpenAI status listing logic here
+    return jsonify({"result": "TODO"})
+
+if __name__ == '__main__':
+    app.run()
